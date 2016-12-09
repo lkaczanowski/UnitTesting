9f9eef1e589790c045595472849e9ae03dbdaaf4
Łukasz Kaczanowski <l.kaczanowski@krd.pl>
2016-12-09 14:20:37 +0100

    2.1 Test double with Moq

---
+++ UnitTesting.Tests/2_TestDouble/DocumentSignerMoqTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+
+using NUnit.Framework;
+
+using UnitTesting._2_TestDouble;
+
+namespace UnitTesting.Tests._2_TestDouble
+{
+  [TestFixture]
+  public class DocumentSignerMoqTests
+  {
+    private DocumentSigner _sut;
+
+    [Test]
+    public void Test_using_moq()
+    {
+      // assign
+      var dummyDocumentRepository = It.IsAny<IDocumentRepository>(); // dummy
+
+      var mockDocumentRepository = new Mock<IDocumentRepository>(); // stub, fake, mock
+
+      var document = new Document
+      {
+        Number = 1
+      };
+
+      mockDocumentRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(document); // stub method
+
+      _sut = new DocumentSigner(mockDocumentRepository.Object);
+
+      // act
+      _sut.Sign(4);
+
+      // assert
+      mockDocumentRepository.Verify(x => x.Update(document));
+    }
+  }
+}

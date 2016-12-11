f1b83170bc1cc66afd630ff9ccf0db4ca59b60f6
Lucas Kaczanowski <lucas.kaczanowski@gmail.com>
2016-12-11 16:48:12 +0100

    3. FsCheck

---
+++ UnitTesting.Tests/3_FsCheck/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using FsCheck;
+
+using NUnit.Framework;
+
+using UnitTesting._3_FsCheck;
+
+namespace UnitTesting.Tests._3_FsCheck
+{
+  [TestFixture]
+  public class CalculatorTests
+  {
+    [Test]
+    public void Test_Sum_properties()
+    {
+      var sut = new Calculator();
+
+      Func<int, int, bool> reverseParamDoesNotMatter = (a, b) => sut.Sum(a, b) == sut.Sum(b, a);
+
+      Func<int, bool> addingZeroToNumberReturnsNumber = a => sut.Sum(a, 0) == a;
+
+      Func<int, bool> addingOneTwiceEqualsAddingTwoOnce = a => sut.Sum(sut.Sum(a, 1), 1) == sut.Sum(a, 2);
+
+      Func<int, int, bool> propertiesOfAdding = (a, b) =>
+        reverseParamDoesNotMatter(a, b) &&
+        addingZeroToNumberReturnsNumber(a) &&
+        addingOneTwiceEqualsAddingTwoOnce(a);
+
+      Prop.ForAll(propertiesOfAdding).QuickCheckThrowOnFailure();
+    }
+  }
+}
---
+++ UnitTesting/3_FsCheck/Calculator.cs
@@ -0,0 +1,10 @@
+namespace UnitTesting._3_FsCheck
+{
+  public class Calculator
+  {
+    public int Sum(int a, int b)
+    {
+      return a + b;
+    }
+  }
+}

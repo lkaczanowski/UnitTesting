f70f462186955993e64b402d805843e2adb841fb
Lucas Kaczanowski <lucas.kaczanowski@gmail.com>
2016-12-11 17:31:53 +0100

    4.3 TDD - Multiplier of 3 and 5 (bad implementation)

--- UnitTesting.Tests/4._TDD/FizzBuzzerTests.cs
+++ UnitTesting.Tests/4._TDD/FizzBuzzerTests.cs
@@ -38,5 +38,20 @@ namespace UnitTesting.Tests._4._TDD
       // assert
       Assert.AreEqual(expected, actual);
     }
+
+    [Test]
+    public void When_number_is_multiplier_of_3_and_5_then_return_FizzBuzz()
+    {
+      // arrange
+      var expected = "FizzBuzz";
+
+      _sut = new FizzBuzzer();
+
+      // act
+      var actual = _sut.FizzBuzz(30);
+
+      // assert
+      Assert.AreEqual(expected, actual);
+    }
   }
 }

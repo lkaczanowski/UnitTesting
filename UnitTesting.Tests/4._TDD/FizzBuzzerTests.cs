87df43ba1c3f2609d303efa7f10c7b5a843f92a3
Lucas Kaczanowski <lucas.kaczanowski@gmail.com>
2016-12-11 17:24:15 +0100

    4.1 TDD - Multiplier of 3

--- UnitTesting.Tests/4._TDD/FizzBuzzerTests.cs
+++ UnitTesting.Tests/4._TDD/FizzBuzzerTests.cs
@@ -8,5 +8,20 @@ namespace UnitTesting.Tests._4._TDD
   public class FizzBuzzerTests
   {
     private FizzBuzzer _sut;
+
+    [Test]
+    public void When_number_is_multiplier_of_3_then_return_Fizz()
+    {
+      // arrange
+      var expected = "Fizz";
+
+      _sut = new FizzBuzzer();
+
+      // act
+      var actual = _sut.FizzBuzz(6);
+
+      // assert
+      Assert.AreEqual(expected, actual);
+    }
   }
 }
--- UnitTesting/4_TDD/FizzBuzzer.cs
+++ UnitTesting/4_TDD/FizzBuzzer.cs
@@ -2,5 +2,14 @@
 {
   public class FizzBuzzer
   {
+    public string FizzBuzz(int number)
+    {
+      if (number % 3 == 0)
+      {
+        return "Fizz";
+      }
+
+      return string.Empty;
+    }
   }
 }
